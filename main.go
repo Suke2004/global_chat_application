ff026975e243b570e550828149e1d9e625e7e455
Suke2006 <lyricsofsongs96@gmail.com>
2024-12-27 00:39:12 +0530

    completed backend

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	customwebsocket "global_chat_application/websocket"
+	"log"
+	"net/http"
+)
+
+func serverWs(pool *customwebsocket.Pool,w http.ResponseWriter, r *http.Request) {{
+	log.Println("websocket is connected")
+	conn, err := customwebsocket.Upgrade(w,r)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	
+	client := *customwebsocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+	pool.Register <- client
+	client.Read()
+}
+
+func setupRoutes{
+	log.Println("routes are established")
+	pool := customwebsocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/api/ws", func(w http.ResponseWriter, r *http.Request){
+		serverWs(pool,w,r)
+	})
+}
+
+func main(){
+	setupRoutes()
+	http.ListenAndServe(":8080",nil)
+}
